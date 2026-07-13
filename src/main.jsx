563b3ff1a78e2190f353469da47fa7c87ba6d6c3
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-07-14 04:26:16 +0800

    Load passage return button styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './devotion-choice.css';
+import './bible-return.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
