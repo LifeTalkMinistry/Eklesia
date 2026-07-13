a41947b15bb85149241490d3cf690a94e3ec9387
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-07-14 03:56:15 +0800

    Load devotion choice styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './devotion-choice.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
