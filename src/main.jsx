ea6b0d094d8e1e515524082a8b9ec43cd8ed0298
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-07-15 04:42:14 +0800

    Prepare Ekklesia Pulse for private alpha testing

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { runBrandMigration } from './services/brandMigration.js';
 import './index.css';
+import './alpha.css';
 import './devotion-choice.css';
 import './bible-return.css';
 import './wgap.css';
